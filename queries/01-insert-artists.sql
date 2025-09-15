6cdb0a2a779a939a5feb344b2c34d30b18c5ba99
Bianca Luisa <ghostekkjk@gmail.com>
2025-09-15 15:58:45 -0300

    cria as querys de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -9,3 +9,8 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN    
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+    ('Venere Vai Vênus', 'Banda', 4, 'MPB', 2020, 'Brasil', 'Circo', TRUE),
+    ('Lil Peep', 'Solo', 1, 'Emo Rap', 2013, 'USA', 'Star Shopping', FALSE)
