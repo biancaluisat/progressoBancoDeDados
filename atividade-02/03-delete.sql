31105a02ed438102843fdfe80ab98bd47093cc41
Bianca <ghostekkjk@gmail.com>
2025-09-29 14:04:46 -0300

    finaliza delete

--- atividade-02/03-delete.sql
+++ atividade-02/03-delete.sql
@@ -1,9 +1,14 @@
 DELETE FROM produtos
-WHERE estoque < 10;
+WHERE estoque < 150;
 
 DELETE FROM produtos
-WHERE preco < 20;
+WHERE preco < 50;
 
 DELETE FROM produtos
 WHERE marca = 'Rare Beauty';
 
+DELETE FROM produtos
+WHERE estoque > 200 AND estoque < 400;
+
+DELETE FROM produtos
+WHERE estoque BETWEEN 150 AND 300;
